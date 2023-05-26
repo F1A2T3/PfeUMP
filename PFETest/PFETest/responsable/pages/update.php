f7b234ecb4f940d802e803ab842ade988bcb92d8
Fati <96390496+F1A2T3@users.noreply.github.com>
2023-05-26 12:01:06 +0100

    Delete PFETest directory

--- PFETest/PFETest/responsable/pages/update.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'connection.php';
-if(isset($_GET['CIN']))
-    $CIN = $_GET['CIN'];
-?>
--- PFETest/Test/function_affiche_tab.php
+++
@@ -1,6 +0,0 @@
-<?php
-function print_r2($tab){
-    echo "<pre>";
-    print_r($tab);
-    echo "</pre>";
-} ?>
